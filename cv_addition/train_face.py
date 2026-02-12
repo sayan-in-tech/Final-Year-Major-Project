fcb8513bdc44e752938c47c45cd7a9bf4bd83621
sayan <sayan18012004@gmail.com>
2026-02-13 02:08:52 +0530

    added notebook

--- cv_addition/train_face.py
+++ cv_addition/train_face.py
@@ -5,7 +5,26 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from PIL import Image
 import os
-from models.face_model import FaceEmotionCNN, EMOTIONS
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class FaceEmotionCNN(nn.Module):
+    def __init__(self, num_classes=7):
+        super(FaceEmotionCNN, self).__init__()
+        # Use MobileNetV2
+        self.mobilenet = models.mobilenet_v2(pretrained=True)
+        # Modify classifier
+        self.mobilenet.classifier = nn.Sequential(
+            nn.Dropout(0.2),
+            nn.Linear(self.mobilenet.last_channel, num_classes)
+        )
+    
+    def forward(self, x):
+        return self.mobilenet(x)
+
+# Emotion labels
+EMOTIONS = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 
 class FaceDataset(Dataset):
     def __init__(self, root_dir, transform=None):
